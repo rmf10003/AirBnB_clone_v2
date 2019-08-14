fda8d42ba8a467535916759dc42600a20944b7ae
OrangeB0lt <667@holbertonschool.com>
2019-08-14 19:26:21 -0400

    fixed mysql scripts

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
 -- sets up a MySQL server for AirBNB clone
 
-CREATE DATABASE IF NOT EXISTS 'hbnb_test_db';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL ON 'hbnb_test_db'.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON 'performance_schema'.* TO 'hbnb_test'@'localhost';

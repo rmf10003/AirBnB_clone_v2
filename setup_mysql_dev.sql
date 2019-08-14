deea228e1ed80596a46738fc951814b96c53c42c
rmf10003 <685@holbertonschool.com>
2019-08-14 23:54:47 +0000

    change dev script sintax

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,5 @@
 -- preps a MySQL sever for project
 
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
+GRANT ALL ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';

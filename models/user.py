c4583e0a227f295c58e1bc77bbf7e04c33bfae93
OrangeB0lt <667@holbertonschool.com>
2019-08-16 01:48:48 +0000

    create user db storage ability

--- models/user.py
+++ models/user.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel, Base, Column, String
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 from os import getenv
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This is the class for user
     Attributes:
         email: email address
@@ -16,15 +16,20 @@ class User(BaseModel):
     """
 
     __tablename__ = "users"
-    # if getenv('HBNB_TYPE_STORAGE') == 'db':
-    #     email = Column(String(128), nullable=False)
-    #     password = Column(String(128), nullable=False)
-    #     first_name = Column(String(128), nullable=True)
-    #     last_name = Column(String(128), nullable=True)
-    #     places = relationship("Place", back_populates="user")
-    #     reviews = relationship("Review", back_populates="user")
-    # else:
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+
+        # places = relationship(
+        #     'Place', back_populates='user',
+        #     cascade='all, delete, delete-orphan')
+        # reviews = relationship(
+        #     'Review', back_populates='user',
+        #     cascade='all, delete, delete-orphan')
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""

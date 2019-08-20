a410dce369d4a5d8312f595a3e99381c0ac7d1f4
rmf10003 <685@holbertonschool.com>
2019-08-20 03:41:22 +0000

    tar file compression

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""generate .tgz archive from contents of web_static folder"""
+
+import fabric.api
+from os.path import isdir
+import os
+from datetime import datetime
+
+def do_pack():
+    """use fabric to generate a .tgz file from the given directory"""
+    if isdir('versions') is False:
+        os.mkdir('versions')
+    time_stamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    file_name = "versions/webstatic_" + time_stamp + ".tgz"
+    fabric.api.local(
+        "tar -cvzf " + file_name + " web_static "
+        )
+    return file_name

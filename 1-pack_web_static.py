4bd72dc7dc232d53992e88d874130d8d6e6fa623
rmf10003 <685@holbertonschool.com>
2019-08-21 01:03:19 +0000

    add - to strftime

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """generate .tgz archive from contents of web_static folder"""
 
-import fabric.api
+from fabric.api import local
 from os.path import isdir
 import os
-import os.path
 from datetime import datetime
 
 
@@ -12,7 +11,7 @@ def do_pack():
     """use fabric to generate a .tgz file from the given directory"""
     if isdir('versions') is False:
         os.mkdir('versions')
-    time_stamp = datetime.now().strftime('%Y%m%d%H%M%S')
-    file = "versions/webstatic_" + time_stamp + ".tgz"
-    fabric.api.local("tar -cvzf {} web_static".format(file))
+    time_stamp = datetime.now().strftime('%Y%-m%d%H%M%S')
+    file = "versions/web_static_" + time_stamp + ".tgz"
+    local("tar -cvzf {} web_static/".format(file))
     return file

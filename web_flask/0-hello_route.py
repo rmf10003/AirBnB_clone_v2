36f5ec96c1d98acd9f87aad885a5825080853182
rmf10003 <685@holbertonschool.com>
2019-08-30 23:35:49 +0000

    make the flask web framework run if its main

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,10 +3,13 @@
 from flask import Flask
 
 
-if __name__ == '__main__':
-    app = Flask(__name__)
 
-    @app.route('/', strict_slashes=False)
-    def hello_hbnb():
-        """say hello"""
-        return 'Hello HBNB!'
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """say hello"""
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')

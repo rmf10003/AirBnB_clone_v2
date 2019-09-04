f854b503ddf8a3cb7baeedc85a0c74ee599ed556
rmf10003 <685@holbertonschool.com>
2019-09-04 17:40:15 +0000

    from flask import Flask

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """start a Flask web app"""
-import flask
+from flask import Flash
 
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)

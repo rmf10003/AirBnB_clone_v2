5c346501db6d3a269185a03069ebfe0d9b31539b
rmf10003 <685@holbertonschool.com>
2019-09-04 00:35:15 +0000

    add review to classes dict

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -18,7 +18,7 @@ class DBStorage:
     """ class for DBstorage """
     __engine = None
     __session = None
-    classes = {'State': State, 'User': User, 'City': City, 'Place': Place}
+    classes = {'State': State, 'User': User, 'City': City, 'Place': Place, 'Review': Review}
 
     def __init__(self):
         """constructor for DBStorage instances
